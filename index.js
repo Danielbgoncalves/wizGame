0c74fb6782af01f62495843208c32560efcd1bc7
Daniel Borges <44danbor@gmail.com>
2024-09-20 17:15:12 -0300

    Adição do mongodb ao projeto

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-
+const {MongoClient} = require('mongodb');
 
 const app = express();
 const port = 3000;
+
+const uri = process.env.MONGODB_URI || 'mongodb://mongo:yvTehVZQAfLeKJItQVWkNyDWkjkhkdvS@junction.proxy.rlwy.net:26846';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Conectado ao MongoDB!');
+  } catch (error) {
+    console.error('Erro ao conectar no MongoDB', error);
+  }
+}
+
+connectToDatabase();
 
 const allowedOrigins = ['http://127.0.0.1:5500', 'https://danielbgoncalves.github.io'];
 
@@ -23,39 +37,44 @@ const corsOption = {
   app.use(cors(corsOption));
   app.use(express.json());
 
-  const ranking = [];
-  app.post('/api/ranking', (req, res) => {
+  //const ranking = [];
+  app.post('/api/ranking', async (req, res) => {
     try{
         let {nome, pontuacao, id} = req.body;
-        let jogadorJaPresente = false;
+        //let jogadorJaPresente = false;
 
         if(!nome || typeof pontuacao != 'number'){
             return res.status(400).json({message: 'nome ou pontuacao invalidos'});
         }
 
-        ranking.forEach((jogador) => {
-            if(jogador.id === id){
-              if(jogador.pontuacao < pontuacao) jogador.pontuacao = pontuacao;
-              jogadorJaPresente = true;
-            }
-        })
+        const db = client.db('ranking');
+        const collection = db.collection('players-ranking');
 
-        if(!jogadorJaPresente){
-          ranking.push({nome, pontuacao, id});
+        const jogador = await collection.findOne({ id });
+
+        if(jogador){
+          if(jogador.pontuacao < pontuacao){
+            await collection.updateOne({ id }, { $set: { pontuacao }});
+          }
+        } else {
+          await collection.insertOne({ nome, pontuacao, id });
         }
         
         res.status(201).json({message: 'Dados enviados com sucesso'});
-
     } catch (error){
-        console.error('Erro ao buscar o ranking no servidor');
+        console.error('Erro ao buscar o ranking no servidor', error);
         res.status(500).json({message: 'Erro do servidor ao processar sacola'});
     }
   });
 
-  app.get('/api/ranking',(req, res) => {
+  app.get('/api/ranking', async (req, res) => {
 
     try{
-        let rankingOrdenado = ranking.sort((a,b) => b.pontuacao - a.pontuacao);
+        const db = client.db('ranking');
+        const collection = db.collection('players-ranking'); 
+
+        const rankingOrdenado = await collection.find().sort({pontuacao:-1}).toArray();
+        
         res.status(200).json(rankingOrdenado);
     } catch(error){
         console.error('Erro ao buscar ranking: ', error);
